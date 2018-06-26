d56da6d19f0d4ba486593bc626e4414441639ea5
Michael Abelar <miabelar@fb.com>
2018-06-26 13:10:02 -0700

    Initial Commit

---
+++ Tippy/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Tippy
+//
+//  Created by Michael Abelar on 6/26/18.
+//  Copyright © 2018 Michael Abelar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
